3cc3e12a632789fc8ce55605e9842a9e8babdae5
glisa97 <aleksandarglisic@outlook.com>
2021-09-17 15:41:39 +0200

    the shop 02 gui update

--- TheShop/TheShop/TheShopGui/MainWindow.xaml.cs
+++ TheShop/TheShop/TheShopGui/MainWindow.xaml.cs
@@ -14,7 +14,7 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using Dapper;
 using Npgsql;
-
+using System.Collections.ObjectModel;
 namespace TheShopGui
 {
     /// <summary>
@@ -24,20 +24,73 @@ namespace TheShopGui
     {
         List<Store> ListOfStores = new List<Store>();
         List<Inventory> ListOfInventories = new List<Inventory>();
-        
+        ObservableCollection<Store> storeList;
+        ObservableCollection<Inventory> InventoryLList;
+
+
 
         public MainWindow()
         {
             InitializeComponent();
-
+            ListOfStores = GetStoresFromDatabase("Beograd");
+            ListOfInventories = GetInventoriesFromDatabase("Maxi");
+            storeList = new ObservableCollection<Store>(ListOfStores);
+            InventoryLList = new ObservableCollection<Inventory>(ListOfInventories);
+            dgridStoresInCity.ItemsSource = storeList;
+            dgridInventory.ItemsSource = InventoryLList;
             
         }
 
+        private List<Inventory> GetInventoriesFromDatabase(string v)
+        {
+            List<Inventory> inventories;
+            string connectionStringInv = "Server=127.0.0.1;Port=5432;Database=shopDb;User Id=postgres;Password=admin;";
+            
+                using (NpgsqlConnection connection = new NpgsqlConnection(connectionStringInv))
+            {
+                string selectFromDatabase = $"SELECT storename, productname, quantity FROM shop.inventory WHERE storename = '{v}'; ";
+
+                inventories = connection.Query<Inventory>(selectFromDatabase).AsList();
+                foreach (Inventory i in inventories)
+                {
+                    Console.Write(i.StoreName + ",");
+                    Console.Write(i.ProductName + ",");
+                    Console.Write(i.Quantity + "\n");
+                    //textbox1.Text += i.StoreName + "," + i.ProductName + "," + i.Quantity + "\n";
+
+                }
+               }
+
+            
+            MessageBox.Show("Store successfully selected");
+            return inventories;
+        }
+
+        private List<Store> GetStoresFromDatabase(string v)
+        {
+            List<Store> stores;
+            string connectionStringcity = "Server=127.0.0.1;Port=5432;Database=shopDb;User Id=postgres;Password=admin;";
+            using (NpgsqlConnection connection = new NpgsqlConnection(connectionStringcity))
+            {
+                string selectFromDatabase = $"SELECT nameofcity, storename, address FROM shop.stores WHERE nameofcity = '{v}'; ";
+                stores = connection.Query<Store>(selectFromDatabase).AsList();
+                foreach (Store s in stores)
+                {
+                    Console.Write(s.NameOfCity + ",");
+                    Console.Write(s.StoreName + ",");
+                    Console.Write(s.Address + "\n");
+
+                }
+
+            }
+            MessageBox.Show("Inventory successfully selected");
+            return stores;
+        }
 
         public void btnAddNewStore_Click(object sender, RoutedEventArgs e)
         {
             Store addNewStoreTemp = new Store(tfCityName.Text, tfStoreName.Text, tfStoreAddress.Text);
-            ListOfStores.Add(addNewStoreTemp);
+            storeList.Add(addNewStoreTemp);
         }
 
         private void btnFinishStoreAdding_Click(object sender, RoutedEventArgs e)
@@ -53,24 +106,7 @@ namespace TheShopGui
                 }
             }
 
-            Console.WriteLine("Enter name of city to search stores in database:");
-            string cityname = Console.ReadLine();
-
-
-            string connectionStringcity = "Server=127.0.0.1;Port=5432;Database=shopDb;User Id=postgres;Password=admin;";
-            using (NpgsqlConnection connection = new NpgsqlConnection(connectionStringcity))
-            {
-                string selectFromDatabase = $"SELECT nameofcity, storename, address FROM shop.stores WHERE nameofcity = '{cityname}'; ";
-                List<Store> stores = connection.Query<Store>(selectFromDatabase).AsList();
-                foreach (Store s in stores)
-                {
-                    Console.Write(s.NameOfCity + ",");
-                    Console.Write(s.StoreName + ",");
-                    Console.Write(s.Address + "\n");
-                    
-                }
-
-            }
+            
         }
        
 
@@ -94,25 +130,15 @@ namespace TheShopGui
             }
 
             
-            string connectionStringInv = "Server=127.0.0.1;Port=5432;Database=shopDb;User Id=postgres;Password=admin;";
-            using (NpgsqlConnection connection = new NpgsqlConnection(connectionStringInv))
-            {
-                string selectFromDatabase = $"SELECT storename, productname, quantity FROM shop.inventory WHERE storename = '{tfStoreName2.Text}'; ";
-
-                List<Inventory> inventoryList = connection.Query<Inventory>(selectFromDatabase).AsList();
-                foreach (Inventory i in inventoryList)
-                {
-                    Console.Write(i.StoreName + ",");
-                    Console.Write(i.ProductName + ",");
-                    Console.Write(i.Quantity + "\n");
-                    textbox1.Text = i.StoreName + "," + i.ProductName + "," + i.Quantity + "\n";
-                    
-                }
-
-            }
+            
         }
 
         private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void dgridStoresInCity_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
 
         }
